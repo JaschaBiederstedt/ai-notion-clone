3c8f4a38e6b7f6c5b58eecfb4088bbfa932e6343
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-11-13 15:45:15 +0100

    Style sidebar menu slider

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,23 +9,35 @@ import {
 import { MenuIcon } from 'lucide-react';
 
 const Sidebar = () => {
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+
+      {/* My Documents */}
+      {/* List */}
+
+      {/* Shared with me */}
+      {/* List... */}
+    </>
+  );
+
   return (
     <div className='p-2 md:p-5 bg-gray-200 relative'>
-      <Sheet>
-        <SheetTrigger>
-          <MenuIcon className='p-2 hover:opacity-30 rounded-lg' size={40} />
-        </SheetTrigger>
-        <SheetContent side='left'>
-          <SheetHeader>
-            <SheetTitle>Menu</SheetTitle>
-            <div>{/* Options */}</div>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-
-      <div className='hidden md:inline'>
-        <NewDocumentButton />
+      <div className='md:hidden'>
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className='p-2 hover:opacity-30 rounded-lg' size={40} />
+          </SheetTrigger>
+          <SheetContent side='left'>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>{menuOptions}</div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
+
+      <div className='hidden md:inline'>{menuOptions}</div>
     </div>
   );
 };
