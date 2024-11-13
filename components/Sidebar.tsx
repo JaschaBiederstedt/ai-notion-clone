16f56276727b991fa1b074bef738883cb07b8573
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-11-13 15:34:34 +0100

    Add sidebar menu slider

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,31 @@
 import NewDocumentButton from '@/components/NewDocumentButton';
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { MenuIcon } from 'lucide-react';
 
 const Sidebar = () => {
   return (
     <div className='p-2 md:p-5 bg-gray-200 relative'>
-      <NewDocumentButton />
+      <Sheet>
+        <SheetTrigger>
+          <MenuIcon className='p-2 hover:opacity-30 rounded-lg' size={40} />
+        </SheetTrigger>
+        <SheetContent side='left'>
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <div>{/* Options */}</div>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+
+      <div className='hidden md:inline'>
+        <NewDocumentButton />
+      </div>
     </div>
   );
 };
