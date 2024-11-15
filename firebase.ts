6c9aecb819054f5c7c2abf90439b5abc42f348db
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-11-15 15:45:18 +0100

    Set up firebase

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBMDp_ZSG0E-xhtH840NSE34oy4wS5F5tQ',
+  authDomain: 'ai-notion-clone-27b67.firebaseapp.com',
+  projectId: 'ai-notion-clone-27b67',
+  storageBucket: 'ai-notion-clone-27b67.firebasestorage.app',
+  messagingSenderId: '770548423335',
+  appId: '1:770548423335:web:57ce9ea3494b5214725982',
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+const db = getFirestore(app);
+
+export { db };
