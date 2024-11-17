2cead6ca0b8740456a96dacf2f46597335a8837c
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-11-17 16:04:45 +0100

    Add new doc btn functionality

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,9 +1,25 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { useTransition } from 'react';
 
 const NewDocumentButton = () => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const handleCreateNewDocument = async () => {
+    startTransition(async () => {
+      const { docId } = await createNewDocument();
+      router.push(`/doc/${docId}`);
+    });
+  };
+
   return (
     <div>
-      <Button>New Document</Button>
+      <Button onClick={handleCreateNewDocument} disabled={isPending}>
+        {isPending ? 'Creating...' : 'New Document'}
+      </Button>
     </div>
   );
 };
