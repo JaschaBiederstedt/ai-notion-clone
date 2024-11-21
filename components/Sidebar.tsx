eae64dde3af58be78e4c829c1a636b508d2df286
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-11-21 16:12:32 +0100

    Add shared with me sidebar functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -89,6 +89,17 @@ const Sidebar = () => {
       </div>
 
       {/* Shared with me */}
+      {groupedData.editor.length > 0 && (
+        <>
+          <h2 className='font-semibold text-sm text-gray-500'>
+            Shared with me
+          </h2>
+          {groupedData.editor.map(doc => (
+            <SidebarOption key={doc.id} id={doc.id} href={`/doc/${doc.id}`} />
+          ))}
+        </>
+      )}
+
       {/* List... */}
     </>
   );
